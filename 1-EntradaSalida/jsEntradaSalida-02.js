5de10f10012508d7c187804c5cfb8533c43fac1a
fedoriv facundo <fedorivfacundo@hotmail.com>
2023-01-31 12:06:55 -0300

    hola

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,11 @@
 /*
+Fedoriv Facundo ejercicio 2 
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+let nombrePersona;  //Declaracion de variable
+	nombrePersona = prompt("Cual es tu nombre?");
+	alert("nombrepersona") ;
 }
 
